c753ac6a4f4d34f6aec8313bcac25aef12298b85
Leave-it-blank <leaveitblank32@gmail.com>
2023-07-13 13:41:20 +0530

    added poll management routes

--- controllers/pollController.ts
+++ controllers/pollController.ts
@@ -1,32 +1,23 @@
 /** source/controllers/example.ts */
 import { Request, Response, NextFunction } from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
- 
-const uri = process.env.MONGO_DB || ``;
+import client from '../libs/mongo_client';
 
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
 //   await client.connect();
 //   const dbName = "ovs";
 //   const collectionName = "polls";
 
-  // Create references to the database and collection in order to run
-  // operations on them.
+// Create references to the database and collection in order to run
+// operations on them.
 //   const database = client.db(dbName);
 //   const collection = database.collection(collectionName);
 
-  /*
-   *  *** INSERT DOCUMENTS ***
-   *
-   * You can insert individual documents using collection.insert().
-   * In this example, we're going to create four documents and then
-   * insert them all in one call with collection.insertMany().
-   */
+/*
+ *  *** INSERT DOCUMENTS ***
+ *
+ * You can insert individual documents using collection.insert().
+ * In this example, we're going to create four documents and then
+ * insert them all in one call with collection.insertMany().
+ */
 //   async function run() {
 //     try {
 //       // Connect the client to the server	(optional starting in v4.7)
@@ -40,11 +31,12 @@ const client = new MongoClient(uri, {
 //     }
 //   }
 //   run().catch(console.dir);
-  
-  
+
+
 const createPoll = async (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
     const { title, options } = req.body;
+    console.log(title, options);
     await client.connect();
     const dbName = "ovs";
     const collectionName = "polls";
@@ -54,21 +46,32 @@ const createPoll = async (req: Request, res: Response, next: NextFunction) => {
     const database = client.db(dbName);
     const collection = database.collection(collectionName);
     const id = Math.floor(Math.random() * 1000000000);
+    console.log(req.body.user)
     try {
         const updateData = await collection.insertOne({
             pollId: id,
             pollTitle: title,
             pollOptions: options,
+            active: true,
+            createdAt: new Date(),
+            pollCreator: req.body.user.username,
+            createdBy: req.body.user.userId,
         });
         console.log(`${updateData.acknowledged} documents successfully inserted.\n`);
-      } catch (err) {
+        return res.status(200).json({
+            message: "Successfully Added poll.",
+            status: "success",
+            pollId: id
+        });
+    } catch (err) {
         console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
-      }
+        return res.status(200).json({
+            message: "unable to create.",
+            status: "error",
+        });
+    }
 
-    return res.status(200).json({
-        data: "Successfully Added poll.",
-        pollId: id
-    });
+
 };
 const fetchPoll = async (req: Request, res: Response, next: NextFunction) => {
     console.log(`Fetching Polls`);
@@ -80,10 +83,10 @@ const fetchPoll = async (req: Request, res: Response, next: NextFunction) => {
         const polls = await client.db(dbName).collection(collectionName).find({}).toArray();
         console.log(polls);
 
-    return res.status(200).json({
-        polls
-    });
-    }catch(err){
+        return res.status(200).json({
+            polls
+        });
+    } catch (err) {
         console.log(err);
     }
 
@@ -96,13 +99,13 @@ const fetchPollById = async (req: Request, res: Response, next: NextFunction) =>
     const id = req.params.id;
     console.log(id);
     try {
-            const poll = await client.db(dbName).collection(collectionName).findOne({pollId: Number(id)});
-            console.log(poll);
-            return res.status(200).json({
-                poll
-            });
-            
-    }catch(err){
+        const poll = await client.db(dbName).collection(collectionName).findOne({ pollId: Number(id) });
+        console.log(poll);
+        return res.status(200).json({
+            poll
+        });
+
+    } catch (err) {
         console.log(err);
     }
 }
@@ -112,27 +115,179 @@ const castVoteById = async (req: Request, res: Response, next: NextFunction) => 
     const id = req.params.id;
     const body = req.body;
     const dbName = "ovs";
-    
-    const collectionName2 = "poll-" + req.params.id;
- 
-    try {
-            const poll = await client.db(dbName).collection(collectionName2).findOne({user: Number(body.user)});
-            if(poll != null ){
-                return res.status(200).json({
-                    data: "Already Voted"
-                });
-            }
-            const addVote = await client.db(dbName).collection(collectionName2).insertOne({
-                userIp: Number(body.user),
-                pollId: body.id,
-                vote: body.vote,
-            });
+    console.log(body);
+    const collectionNamepollByid = "poll-" + req.params.id;
+    const collectionNamePolls = "polls";
 
-            return res.status(200).json({
-                data: addVote.acknowledged
+    try {
+        const getPoll = await client.db(dbName).collection(collectionNamePolls).findOne({ pollId: Number(id) });
+        console.log(getPoll);
+        if (getPoll == null) {
+            return res.status(404).json({
+                message: "Poll not found",
+                status: "error"
             });
-        }catch(err){
-            console.log(err);
+        } else if (getPoll.active == false) {
+            return res.status(403).json({
+                message: "Poll is not active",
+                status: "error"
+            });
         }
+
+
+        const query = { user_id: body.user_id, pollId: body.id };
+        const poll = await client.db(dbName).collection(collectionNamepollByid).findOne(query);
+        if (poll != null) {
+            return res.status(203).json({
+                message: "Already Voted",
+                status: "error"
+            });
+        }
+
+        const data = {
+            userIp: String(body.user_ip),
+            pollId: body.id,
+            vote: body.vote,
+            user_id: body.user_id,
+            createdAt: new Date(),
+
+        }
+        console.log(data);
+        const addVote = await client.db(dbName).collection(collectionNamepollByid).insertOne(data);
+
+        return res.status(200).json({
+            message: "Added your vote.",
+            status: "success"
+        });
+    } catch (err) {
+        console.log(err);
     }
-export default {   createPoll, fetchPoll , fetchPollById , castVoteById};
+
+}
+
+const fetchVotesById = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Fetching Votes`);
+    await client.connect();
+    const dbName = "ovs";
+    const collectionName = "poll-" + req.params.id;
+    const id = req.params.id;
+    console.log(id);
+    try {
+        const votes = await client.db(dbName).collection(collectionName).find({}).toArray();
+        console.log(votes);
+        return res.status(200).json({
+            votes: votes.length
+        });
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+const fetchPollResultsById = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Fetching Polls data by id result`);
+    await client.connect();
+    const dbName = "ovs";
+    const collectionName = "poll-" + req.params.id;
+    const collectionNamePolls = "polls";
+    const id = req.params.id;
+    console.log(id);
+    try {
+        const poll = await client.db(dbName).collection(collectionNamePolls).findOne({ pollId: Number(id) });
+        const votes = await client.db(dbName).collection(collectionName).find({}).toArray();
+        console.log(poll);
+
+        const pollOptionCount : any= {};
+
+        votes.forEach((item : any , index: number) => {
+            if (item.vote) {
+                const votedata = item.vote;
+                console.log(votedata)
+                pollOptionCount[votedata] =   pollOptionCount[votedata] ? pollOptionCount[votedata] + 1 : 1  
+            }
+        });
+        if(poll){
+            poll.pollOptions.forEach((option : string) => {
+                if (!pollOptionCount.hasOwnProperty(option[0])) {
+                  pollOptionCount[option[0]] = 0;
+                }
+              });
+        }
+      
+
+        // Sort the poll options based on the count
+        const sortedOptions = Object.entries(pollOptionCount).sort(
+            (a: any, b: any) => b[1] - a[1]
+        );
+        let votesItem = {
+
+        }
+    
+   
+
+        return res.status(200).json({
+            poll: poll,
+            votes: sortedOptions,
+            status: "success",
+            message: "successfully fetched."
+        });
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(401).json({
+            message: err,
+            status: "error"
+        });
+
+
+    }
+}
+
+const fetchActivePolls = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Fetching Active Polls`);
+    await client.connect();
+    const dbName = "ovs";
+    const collectionName = "polls";
+    try {
+        const polls = await client.db(dbName).collection(collectionName).find({ active: true }).toArray();
+        console.log(polls);
+        
+        return res.status(200).json({
+            polls
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+const markPollInactive = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`Marking  Poll inactive`);
+    await client.connect();
+    const dbName = "ovs";
+    const collectionName = "polls";
+    const id = req.params.id;
+    console.log(id);
+    try {
+        const poll = await client.db(dbName).collection(collectionName).findOne({ pollId: Number(id) });
+        console.log(poll);
+        if (poll == null) {
+            return res.status(404).json({
+                message: "Poll not found",
+                status: "error"
+            });
+        }
+        const query = { pollId: Number(id) };
+        const update = { $set: { active: false } };
+        const options = { upsert: true };
+        const result = await client.db(dbName).collection(collectionName).updateOne(query, update, options);
+        console.log(result);
+        return res.status(200).json({
+            message: "Poll marked inactive",
+            status: "success"
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
+export default { createPoll, fetchPoll, fetchPollById, castVoteById, fetchVotesById, fetchPollResultsById, fetchActivePolls , markPollInactive };
--- routes/index.ts
+++ routes/index.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 import PollController from '../controllers/pollController';
 const router = express.Router();
- 
+import {auth} from '../middleware/auth';
 
-router.post('/create-poll', PollController.createPoll);
+router.post('/create-poll' , auth, PollController.createPoll);
 router.get('/polls', PollController.fetchPoll);
 router.get('/poll/:id', PollController.fetchPollById);
-
-router.post('/poll/:id/vote', PollController.castVoteById);
-
-// router.get('/test1',  PollController.test1);
-// router.get('/test2', auth, PollController.test2);
-// router.get('/fail1', auth, PollController.fail1);
-// router.get('/fail2', auth, PollController.fail2);
+router.post('/poll/:id/vote', auth, PollController.castVoteById);
+router.get('/poll/:id/results', PollController.fetchVotesById);
+router.get('/poll/:id/final_results', auth, PollController.fetchPollResultsById);
+router.get('/active-polls', PollController.fetchActivePolls);
+router.get('/poll/:id/mark-inactive', auth, PollController.markPollInactive);
 //@ts-ignore
 export = router;
